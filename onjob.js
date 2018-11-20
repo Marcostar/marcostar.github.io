1a327a2a9cfcd95eaafc88a4b4bb28f739a68c66
Leo <sghadge1@binghamton.edu>
2018-11-20 18:07:39 -0500

    Test Success-1

--- onjob.js
+++ onjob.js
@@ -1,35 +1,64 @@
 let Parser = require('rss-parser');
 let parser = new Parser();
 
-async () => {
+var feed;
 
-    let feed = await parser.parseURL('https://binghamton.joinhandshake.com/external_feeds/2631/public.rss?token=2XhufjIYzTcXOYoWyCgu9_BNP6XMi0G99d9Hx_-pLZLL4RcFK-K5lw');
-    
-    var numberOfListItems = feed.items.length;
-    document.body.innerHTML = "<h1>Today's date is " + feed.items.length + "</h1>";
-  
-    //console.log("Count: "+ feed.items.length);
-    //console.log(feed.title);
-  
-    let list = document.getElementsByTagName('ul')[0];
+function feedTheList()
+{
+
+
+    console.log(feed.items.length);
+    var list = document.getElementById("list");
 
     feed.items.forEach(item => {
-      // console.log(item.title);
-      // console.log(" ");
-      // console.log(item.link);
-      // console.log(" ");
-  
-  
-        var listItem = document.createElement("li");
-  
-        // Add the item text
-        var newText = document.createTextNode(item.title);
-        listItem.appendChild(newText);
-        //listItem.innerHTML =item.title;
-  
-        // Add listItem to the listElement
-        list.appendChild(listItem);
+      
+        console.log(" " + item.title);
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(item.title));
+        list.appendChild(li);
   
     });
-  
-  };
+
+}
+
+const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+
+(async () => {
+  console.log("Messi is legend");
+  feed = await parser.parseURL(CORS_PROXY + 'https://binghamton.joinhandshake.com/external_feeds/2631/public.rss?token=2XhufjIYzTcXOYoWyCgu9_BNP6XMi0G99d9Hx_-pLZLL4RcFK-K5lw');
+  feedTheList();
+})();
+
+//feedTheList();
+// setTimeout(function() {
+//   //your code to be executed after 1 second
+//   feedTheList();
+
+// }, 5000);
+
+
+
+
+
+
+// let Parser = require('rss-parser');
+// let parser = new Parser();
+
+// let feed;
+// (async () => {
+
+//   feed = await parser.parseURL('https://binghamton.joinhandshake.com/external_feeds/2631/public.rss?token=2XhufjIYzTcXOYoWyCgu9_BNP6XMi0G99d9Hx_-pLZLL4RcFK-K5lw');
+//   console.log(feed.items.length);
+
+// })();
+
+
+
+// setTimeout(function() {
+//   //your code to be executed after 1 second
+
+//   feed.items.forEach(item => {
+//     console.log(item.title + ':' + item.link)
+//   });
+
+// }, 3000);
